70c1286326552f9027f43c64daffdf372c1c222c
moussa Traore <moutra213@gmail.com>
2026-08-15 22:52:48 +0200

    ajout de supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tolon/cor/router/routes.dart';
 import 'package:tolon/cor/theme/app_theme.dart';
 import 'package:tolon/firebase_options.dart';
@@ -11,6 +12,10 @@ void main() async {
   // Initialisation obligatoire de votre projet Firebase 'ankatolon' lié à l'étape précédente
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: 'https://zoagjvcjrolrrlhdkhob.supabase.co',
+    anonKey: 'sb_publishable_KDK3Dxx_1XfarmHK1CI5YA_c4aRncjy',
   );
   
   runApp(
