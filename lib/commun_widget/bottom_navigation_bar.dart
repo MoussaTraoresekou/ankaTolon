93c71ec2373d15e480a54bf3aa423e9a89e9dca3
MoussaTraore <moutra213@gmail.com>
2026-08-24 12:13:06 +0000

    dependance

--- lib/commun_widget/bottom_navigation_bar.dart
+++ lib/commun_widget/bottom_navigation_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:tolon/cor/theme/app_theme.dart';
 import 'package:tolon/pages/catalogue/catalogue.dart';
 import 'package:tolon/pages/page_to_delete.dart';
