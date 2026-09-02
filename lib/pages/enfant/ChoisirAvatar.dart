415233e0c0882fbbe56e367ebef9eda38173f2b3
MoussaTraore <moutra213@gmail.com>
2026-09-02 17:45:19 +0000

    avant

--- lib/pages/enfant/ChoisirAvatar.dart
+++ lib/pages/enfant/ChoisirAvatar.dart
@@ -35,7 +35,7 @@ class _ChoisirAvatarScreenState extends ConsumerState<ChoisirAvatarScreen> {
     'assets/images/avatars/avatar9.jpg',
     'assets/images/avatars/avatar10.jpg',
     'assets/images/avatars/avatar11.jpg',
-    'assets/images/avatars/avatar11.Jpg',
+    'assets/images/avatars/avatar12.jpg',
   ];
 
   late String _selectedAvatar;
